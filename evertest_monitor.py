987dd810805310aae34ac6ec557b84605eef24e6
Jan Riedel <jan.riedel@everbase.net>
2014-12-19 19:44:02 +0100

    Threaded every monitored VM, now showing IP, name and time

--- evertest_monitor.py
+++ evertest_monitor.py
@@ -42,7 +42,7 @@ from evertest_netcfg import *
 #--------------------------------------------------------------------------------------
 # Set EVETEST_DEBUG_LEVEL TO - 0: Short debug message; 1: explicit debug message
 #--------------------------------------------------------------------------------------
-EVERTEST_DEBUG_LEVEL = 0
+EVERTEST_DEBUG_LEVEL = 1
 edl = EVERTEST_DEBUG_LEVEL
 #--------------------------------------------------------------------------------------
 # EOF Debug-Settings
@@ -53,7 +53,7 @@ edl = EVERTEST_DEBUG_LEVEL
 # Function receiving the live status from all running VMs (success, fail..)
 # 	-> have to be sorted and analyzed / maybe over 2. module in another process and then passing to core 
 #--------------------------------------------------------------------------------------------------------
-def evertestReceiveStatus(threadName):
+def evertestReceiveStatus(hostname, port):
 	try:
 		while (cnt == 0):
 #			print "Started a new server-socket."
@@ -61,11 +61,12 @@ def evertestReceiveStatus(threadName):
 			message = "Got status."
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # after conn.close() the used socket has been left in TIME_WAIT state. The SO_REUSEADDR option tells the kernel to reuse the socket in TIME_WAIT state.
-			s.bind(("192.168.0.223", EVERTEST_MONITOR_PORT))		# Otherwise it would need its time until the socket can be reused and throw errors.
+			s.bind(("192.168.0.223", (EVERTEST_MONITOR_PORT + port)))		# Otherwise it would need its time until the socket can be reused and throw errors.
 			s.listen(1)
 
 			conn, addr = s.accept()
-			print "Received status from " + str(addr) + " [" + threadName + "] {" + time.strftime("%H:%M:%S") + "}"
+			print "Received status from " + str(addr) + " [" + hostname + "] {" + time.strftime("%H:%M:%S") + "}"
+			print "The hostname is " + hostname
 			while 1:
 				data = conn.recv(buffer_size)
 				if not data: break
@@ -87,22 +88,28 @@ def evertestReceiveStatus(threadName):
 #--------------------------------------------------------------------------------------------------------
 # Monitor main function; not sure if really needed yet 
 #--------------------------------------------------------------------------------------------------------
-def evertestMonitorMain(test, xmlPath):
+def evertestMonitorMain(test, xmlPath, port):
 	try:
 		path = xmlPath
 		root = xmltree.parse(path).getroot()
 		for child in root:
 			if(child.tag == "vm"):
 				hostname = child.get("name")
+				print "der hostname der VM ist " + hostname
 				ip = evertestGetVmIpAddr(test, hostname)
-				thread.start_new_thread(evertestReceiveStatus, (hostname)) 
+				print "The VMs IP is " + ip
+				thread.start_new_thread(evertestReceiveStatus, (hostname, port,))
+				port = port + 1
 	except:
 		e = sys.exc_info()[edl]
 		print "Error in evertestMonitorMain: \n" + str(e)
+	while 1: pass
 #--------------------------------------------------------------------------------------------------------
 # EOF evertestMonitorMain
 #--------------------------------------------------------------------------------------------------------
 
 cnt = 0
+port = 0
+evertestMonitorMain("xfer_file", "/var/evertest/tests/xfer_file/xfer_file.conf", port)
 #while(cnt == 0):	### Queue construct or other waiting-function needed, otherwise errOut will not stop..
-evertestReceiveStatus("barfoo")
+#evertestReceiveStatus("barfoo")
