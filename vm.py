871c9e962f3a9ecc10cdd5a19ba353cfd6c86b3f
Jan Riedel <jan.riedel@everbase.net>
2015-07-22 11:01:41 +0200

    Better logging

--- vm.py
+++ vm.py
@@ -10,18 +10,7 @@ import tarfile
 from subprocess import check_output
 
 ###############
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# create handler for filelogging
-handler = logging.FileHandler('vm.log')
-handler.setLevel(logging.INFO)
-
-# create a logging format
-formatter = logging.Formatter('%(levelname)s - %(name)s - %(message)s')
-handler.setFormatter(formatter)
-
-logger.addHandler(handler)
+logging.basicConfig(format='%(levelname)s : %(name)s : %(message)s', filename="vm.log", level=logging.INFO)
 ###############
 
 workingDir = "/mnt/"
@@ -29,83 +18,91 @@ filesPath  = "/mnt/"
 
 while os.path.isfile("/mnt/spruce_util.py") != True: #util file is the last one being sent to the vm
 	time.sleep(1)
-
 try:
-	from spruce_util import *
-except:
-	e =sys.exc_info()[1]
-	logger.error("Cannot import spruce_util!")
-	sys.exit()
-
-def evertestExtractTest(dottest, testname):
 	try:
-		extractString = "/mnt/" + dottest
-		extractPath = "/mnt/" + testname + "/"
-		tfile = tarfile.open(extractString)
-		if tarfile.is_tarfile(extractString):
-			tfile.extractall(extractPath)
-		else:
-			logger.error("%s is no tarfile or does not exist!", extractString)
-			raise IOError
-	except IOError:
-		logger.error("Tarfile not found, aborting!")
-		evertestSendStatus("[error] - Tarfile not found, aborting!")
-		evertestSendStatus("[finish]")
-		sys.exit()
+		from spruce_util import *
 	except:
-		e = sys.exc_info()[1]
-		logger.error("Error in evertestExtractTest: \n %s", str(e))
+		e =sys.exc_info()[1]
+		logging.error("Cannot import spruce_util!")
+		raise
 
-getHostname = "hostname"
-hostname = check_output(getHostname)
-hostname = hostname.replace('\n', '')
+	def evertestExtractTest(dottest, testname):
+		try:
+			extractString = "/mnt/" + dottest
+			extractPath = "/mnt/" + testname + "/"
+			tfile = tarfile.open(extractString)
+			if tarfile.is_tarfile(extractString):
+				tfile.extractall(extractPath)
+			else:
+				logging.error("%s is no tarfile or does not exist!", extractString)
+				raise IOError
+		except IOError:
+			logging.error("Tarfile not found, aborting!")
+			raise
+		except:
+			e = sys.exc_info()[1]
+			logging.error("Error in evertestExtractTest: \n %s", str(e))
+			raise
 
-dottest = check_output(["ls -LR /mnt/ | grep *.tar*"], shell=True)
-dottest = dottest.replace('\n', '')
-testname = check_output(["ls -LR /mnt/ | grep *.tar* | cut -d'.' -f 1"], shell=True)
-testname = testname.replace('\n', '')
+	getHostname = "hostname"
+	hostname = check_output(getHostname)
+	hostname = hostname.replace('\n', '')
 
-evertestExtractTest(dottest, testname)
+	dottest = check_output(["ls -LR /mnt/ | grep *.tar*"], shell=True)
+	dottest = dottest.replace('\n', '')
+	testname = check_output(["ls -LR /mnt/ | grep *.tar* | cut -d'.' -f 1"], shell=True)
+	testname = testname.replace('\n', '')
 
-#Script File
-source = "/mnt/{}/scripts/{}.script".format(testname, hostname)
-if (os.path.exists(source)):
-	shutil.copy(source, workingDir)
-else:
-	logger.error("Script file cannot be found!")
-	evertestSendStatus("[error] - Script file not found, aborting!")
-	evertestSendStatus("[finish]")
-	sys.exit()
+	evertestExtractTest(dottest, testname)
 
-#Config File
-config = "/mnt/{0}/{0}.conf".format(testname)
-if (os.path.exists(config)):
-	shutil.copy(config, workingDir)
-else:
-	logger.error("Config file cannot be found!")
-	evertestSendStatus("[error] - Config file not found, aborting!")
-	evertestSendStatus("[finish]")
-	sys.exit()
+	#Script File
+	source = "/mnt/{}/scripts/{}.script".format(testname, hostname)
+	if (os.path.exists(source)):
+		shutil.copy(source, workingDir)
+	else:
+		logging.error("Script file cannot be found!")
+		raise
 
-#Other files
-fileList = os.listdir("/mnt/{}/files/".format(testname))
-fileList = ["/mnt/{}/files/".format(testname) + filename for filename in fileList]
+	#Config File
+	config = "/mnt/{0}/{0}.conf".format(testname)
+	if (os.path.exists(config)):
+		shutil.copy(config, workingDir)
+	else:
+		logging.error("Config file cannot be found!")
+		raise
 
-for f in fileList:
-    shutil.copy2(f, filesPath)
+	#Other files
+	fileList = os.listdir("/mnt/{}/files/".format(testname))
+	fileList = ["/mnt/{}/files/".format(testname) + filename for filename in fileList]
 
-#Execute script
-scriptFile = "/mnt/" + hostname + ".script"
+	for f in fileList:
+	    shutil.copy2(f, filesPath)
 
-runs = "python -B " + scriptFile
-try:
-	logger.info("Trying to execute test script")
-	run = sub.Popen(runs, shell=True) #Changed sub.PIPE to sys.stdout referring to http://stackoverflow.com/a/20196781 -> broken Pipe [32] Error fix
-	run.communicate()
-	#evertestSendStatus("[finish]")
+	#Execute script
+	scriptFile = "/mnt/" + hostname + ".script"
+
+	runs = "python -B " + scriptFile
+	try:
+		logging.info("Trying to execute test script")
+		run = sub.Popen(runs, shell=True) #Changed sub.PIPE to sys.stdout referring to http://stackoverflow.com/a/20196781 -> broken Pipe [32] Error fix
+		out = run.communicate()
+		logging.info(out[0])
+		if out[1]:
+			logging.error(out[1])
+		logging.info("initialized server")
+		#evertestSendStatus("[finish]")
+	except:
+		logging.error("Error on executing '{}'!".format(runs))
+		raise
+
 except:
-	logger.error("Error on executing '{}'!".format(runs))
-	evertestSendStatus("[error] - Error on executing '{}'!".format(runs))
+	e = sys.exc_info()[1]
+	logging.error("Reraised exception: {}".format(e))
+
+finally:
+	with open("vm.log") as l:
+		for line in l:
+			evertestSendStatus("[info] - " + line)
 	evertestSendStatus("[finish]")
-#run.wait()
+	sys.exit()
 
